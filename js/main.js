d2dbea35e6e177d8ab7aae1313ef8171b149d32c
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-18 22:57:48 +0200

    Zadanie37

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function AddText()
+{
+    let d=document.getElementById("text").textContent;
+    d.textContent="Akademia 108";
+    
+}
+function DeleteText()
+{
+   let u= document.getElementById("text").textContent;
+   u.textContent='';
+}
+
+/*
+var delMe = document.getElementById('text');
+    delMe.innerHTML = '';
+}
+function insert()
+{
+    var insMe = document.getElementById('text');
+    insMe.innerHTML = "Line 1\r\nLine2";*/
